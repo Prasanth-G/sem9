e935c8885304e6617d3f6fafc1c4c862d3de765b
15PT25@cs2k16.amcs.ws2k12.psgcs.edu <15PT25@cs2k16.amcs.ws2k12.psgcs.edu>
2019-07-16 11:54:12 +0530

    16-july

--- IR/WS_1/1.py
+++ IR/WS_1/1.py
@@ -1,5 +1,11 @@
 import re, os
 from nltk.stem import PorterStemmer
+from os import listdir
+from os.path import isfile, join
+from collections import defaultdict
+
+home_dir = "Z:\\data"
+onlyfiles = [f for f in listdir(home_dir) if isfile(join(home_dir, f)) and f.endswith(".txt")]
 
 tokenizer = re.compile('\W')
 def getTokensStream(stream):
@@ -12,10 +18,21 @@ def stemmer(stream):
         yield ps.stem(each)
 
 if __name__ == '__main__':
-    filepath = os.path.join(os.getcwd(), r'sample.txt')
-    with open(filepath) as file:
-        tokenStream = getTokensStream(file)
-        for line in tokenStream:
-            for word in stemmer(line):
-                print(word)
-
+    for file_ in onlyfiles:
+        filepath = os.path.join(os.getcwd(), file_)
+        index = 0
+        file_index = []
+        file_terms = defaultdict(lambda : set())
+        term_index = defaultdict(lambda : [])
+        with open(filepath) as file:
+            if filepath not in file_index:
+                file_index.append(filepath)
+                index = index + 1
+            tokenStream = getTokensStream(file)
+            for line in tokenStream:
+                for word in stemmer(line):
+                    term_index[word].add(index)
+                    file_terms[index].add(word)
+    print(file_index)
+    print(file_terms)
+    print(term_index)
