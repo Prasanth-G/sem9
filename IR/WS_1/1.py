0f7c8896bf0bac870d3cff0f510692b0141831bc
15PT25@cs2k16.amcs.ws2k12.psgcs.edu <15PT25@cs2k16.amcs.ws2k12.psgcs.edu>
2019-07-20 14:04:30 +0530

    20-july

--- IR/WS_1/1.py
+++ IR/WS_1/1.py
@@ -3,9 +3,10 @@ from nltk.stem import PorterStemmer
 from os import listdir
 from os.path import isfile, join
 from collections import defaultdict
+from pprint import pprint
 
 home_dir = "Z:\\data"
-onlyfiles = [f for f in listdir(home_dir) if isfile(join(home_dir, f)) and f.endswith(".txt")]
+onlyfiles = [join(home_dir, f) for f in listdir(home_dir) if isfile(join(home_dir, f)) and f.endswith(".txt")]
 
 tokenizer = re.compile('\W')
 def getTokensStream(stream):
@@ -18,21 +19,22 @@ def stemmer(stream):
         yield ps.stem(each)
 
 if __name__ == '__main__':
-    for file_ in onlyfiles:
-        filepath = os.path.join(os.getcwd(), file_)
-        index = 0
-        file_index = []
-        file_terms = defaultdict(lambda : set())
-        term_index = defaultdict(lambda : [])
+    index = 0
+    file_index = []
+    file_terms = defaultdict(lambda : set())
+    term_index = defaultdict(lambda : set())
+    for filepath in onlyfiles:
+        #filepath = os.path.join(os.getcwd(), file_)
+        print(filepath)
         with open(filepath) as file:
             if filepath not in file_index:
                 file_index.append(filepath)
-                index = index + 1
             tokenStream = getTokensStream(file)
             for line in tokenStream:
                 for word in stemmer(line):
                     term_index[word].add(index)
                     file_terms[index].add(word)
-    print(file_index)
-    print(file_terms)
-    print(term_index)
+            index = index + 1
+    pprint(file_index)
+    pprint(file_terms)
+    pprint(term_index)
